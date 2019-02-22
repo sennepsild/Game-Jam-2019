7351476ffe5f325fe609da1141df262a3c19b4e2
Magnus <magnusmhp@gmail.com>
2019-02-22 21:50:00 +0100

    my stuff

---
+++ City Fight unity folder/Assets/Scripts/CityManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CityManager : MonoBehaviour
+{
+    public Text PopulationScoreText,FoodScoreText,WealthScoreText,PowerScoreText;
+    public float PopulationScore, FoodScore, WealthScore, PowerScore;
+
+    List<Unit> Inhabitants = new List<Unit>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddInhabitant(new Unit());
+        UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateScore()
+    {
+        PopulationScoreText.text = PopulationScore + "";
+        FoodScoreText.text = FoodScore + "";
+        WealthScoreText.text = WealthScore + "";
+        PowerScoreText.text = PowerScore + "";
+    }
+
+
+
+    public void AddInhabitant(Unit unit)
+    {
+        Inhabitants.Add(unit);
+        PopulationScore += unit.Population;
+
+    }
+}
---
+++ City Fight unity folder/Assets/Scripts/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit 
+{
+    public float Population = 1;
+    public bool HasBetweenTurnEvent = false;
+    public string UnitClass = "King";
+    public string UnitRace = "Human";
+
+
+    public Unit()
+    {
+
+    }
+
+
+    public void SetupUnit()
+    {
+
+    }
+
+    
+
+    public virtual void specialEvent()
+    {
+
+    }
+
+
+    public virtual void BetweenTurnEvent()
+    {
+
+    }
+
+
+}
